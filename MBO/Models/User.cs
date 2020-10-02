c5c1268c0438fe1799183bc9dc8fcf1182aed12a
PERRODON <alexandre@perrodon.com>
2020-10-02 10:12:27 +0200

    Version 0
    Début projet

---
+++ MBO/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBO.Models
+{
+    public class User
+    {
+        private string NomUtilisateur { get; set; }
+        private string PrenomUtilisateur { get; set; }
+        private DateTime DateCreationUtilisateur { get; set; }
+        private string LoginUtilisateur { get; set; }
+        private string MotDePasseUtilisateur { get; set; }
+    }
+}
---
+++ MBO/Views/Login.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using MBO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBO.Views
+{
+    public class Login
+    {
+
+        [BindProperty] public User User { get; set; }
+
+        public Login()
+        {
+
+        }
+
+        protected IActionResult RenderPage()
+        {
+            return null;
+        }
+
+        protected IActionResult PostData()
+        {
+            //APPEL A UNE ROUTE API
+            return null;
+        }
+    }
+}
